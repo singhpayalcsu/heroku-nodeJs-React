7cc6c0e55f1795eafa6df5fd1a8f9bf79f3e7d1a
Payal Singh <singhpayalsp@Payals-MacBook-Pro.local>
2021-04-23 20:59:48 -0700

    config port

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
+var path = require('path');
 const cors = require('cors')
 const bodyParser = require('body-parser')
 //const fs = require('fs')
 var multer  = require('multer')
 var upload = multer({ dest: 'uploads/' })
 
-
+//app.listen(process.env.PORT || 5000);
 
 //require('dotenv').config()
 // Load the AWS SDK for Node.js
@@ -20,6 +21,10 @@ app.use(cors())
 app.use(express.json())
 
 app.use(bodyParser.urlencoded({extended: true}))
+// ---------------- ADD THIS ----------------
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+// --------------------------------
 /*
 
 
